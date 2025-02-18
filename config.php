2b895776e9574326f55e4a40b8dfce505ca9f76c
Juan Manuel González de la Cruz <153009263+juanmagzl-dev@users.noreply.github.com>
2025-02-18 16:17:45 +0100

    Add files via upload

--- config.php
+++ config.php
@@ -1,11 +1,12 @@
 <?php
-$host = getenv('DB_HOST');
-$dbname = getenv('DB_NAME');
-$user = getenv('DB_USER');
-$password = getenv('DB_PASSWORD');
+$host = getenv('mysql.railway.internal');
+$dbname = getenv('railway');
+$user = getenv('root');
+$password = getenv('HCpgRfXCDwtalklAkzyoMPCsjSzfMnDK');
+$port = getenv('3306');
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $user, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Error en la conexión: " . $e->getMessage());
